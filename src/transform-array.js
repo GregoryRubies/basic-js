1f7d319e6be4c20dd9be775082df86adb825e057
gregoryrubies <GregoryRubies@gmail.com>
2023-04-18 04:24:21 +0600

    feat: transform - create getSumOfDigits function. Date 18.04.2023 04:24

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,21 +1,46 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Create transformed array based on the control sequences that original
- * array contains
- * 
- * @param {Array} arr initial array
- * @returns {Array} transformed array
- * 
- * @example
- * 
- * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
- * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
- */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if(Array.isArray(arr)){
+    let 
+      newArr = new Array(...arr),
+      i = 0,
+      l = newArr.length;
+    while(i < l){
+      switch(newArr[i]){
+        case '--discard-prev': 
+          if(i > 0){ 
+            newArr.splice(i - 1, 1);
+          } else {
+            newArr.shift();
+          } break;
+        case '--double-prev': 
+          if(i > 0){ 
+            newArr[i] = newArr[i-1];
+            ++i;
+          } else {
+            newArr.shift();
+          } break;
+        case '--discard-next': 
+          if(i < l - 1){
+            newArr.splice(++i, 1);
+          } else {
+            newArr.pop();
+          } break;
+        case '--double-next': 
+          if(i < l - 1){
+            newArr[i] = newArr[i + 1];
+          } else {
+            newArr.pop();
+          } break;
+        default: ++i;
+      }
+      l = newArr.length;
+    }
+    return newArr.filter(v => v != '--discard-prev' && v != '--double-prev' && v != '--discard-next' && v != '--double-next');
+  } else {
+    throw Error("\'arr\' parameter must be an instance of the Array!");
+  }
 }
 
 module.exports = {
