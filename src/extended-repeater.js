e68a31e4eeb3c58bcfa21d296ad52942dbe91a13
gregoryrubies <GregoryRubies@gmail.com>
2023-04-17 20:01:09 +0600

    feat: repeater -create repeater function. Date 17.04.2023 20:01

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
 
+function repeater(str, options) {
+
+    if(! ('repeatTimes' in options)) options.repeatTimes = 1;
+    if(! ('separator' in options)) options.separator = '+';
+    if(! ('addition' in options)) options.addition = '';
+    if(! ('additionRepeatTimes' in options)) options.additionRepeatTimes = 1;
+    if(! ('additionSeparator' in options)) options.additionSeparator = '|';
+
+  return new Array(options.repeatTimes)
+    .fill(
+      str + new Array(options.additionRepeatTimes)
+        .fill(String(options.addition))
+          .join(String(options.additionSeparator))
+    ).join(String(options.separator));
+}
 module.exports = {
   repeater
 };
