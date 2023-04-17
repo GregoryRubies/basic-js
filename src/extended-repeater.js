f3ee2b9130b494e8444aae69654281fe6b70c68f
gregoryrubies <GregoryRubies@gmail.com>
2023-04-17 21:23:31 +0600

    feat: renameFiles -create renameFiles function. Date 17.04.2023 21:23

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,21 +1,5 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Create a repeating string based on the given parameters
- *  
- * @param {String} str string to repeat
- * @param {Object} options options object 
- * @return {String} repeating string
- * 
- *
- * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
- * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
- * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
- *
- */
-
 function repeater(str, options) {
 
     if(! ('repeatTimes' in options)) options.repeatTimes = 1;
