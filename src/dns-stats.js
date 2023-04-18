8e950cb7cfcf3ec41fb0cbc53688e5fcc3816cd9
gregoryrubies <GregoryRubies@gmail.com>
2023-04-18 06:10:22 +0600

    feat: getDNSStats - create getDNSStats function. Date 18.04.2023 06:10

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let result = {};
+  domains.flatMap(v => 
+    v.split('.')
+      .reduceRight((acc, v, i, a)=> {
+        acc.push(acc.slice(acc.length - 1) + '.' + v); 
+        return acc;
+      }, 
+      [])
+  ).forEach(
+    function(v){
+        this[v] = v in this ? this[v] + 1 : 1;
+    }, 
+    result
+  )
+  return result;
 }
 
 module.exports = {
